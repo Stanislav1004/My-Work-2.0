a482528c6c40fb7125e73a9a4da5eb7c0b33ce38
Stanislav1004 <op7191221@gmail.com>
2026-08-22 00:37:13 +0300

    v1.1: Sait for programist

---
+++ main.js
@@ -0,0 +1,15 @@
+let user = prompt('You teacher');
+
+if (user == null) {
+    alert('dilbiib');
+    console.log('dilbiib')
+} else if (user == 'no' || user == 'No') {
+    alert('piridurack');
+    console.log('piridurack')
+} else if (user == 'yes' || user == 'Yes') {
+    alert('Yes you teacher');
+    console.log('Yes you teacher');
+} else {
+    alert('What');
+    console.log('none')
+}
